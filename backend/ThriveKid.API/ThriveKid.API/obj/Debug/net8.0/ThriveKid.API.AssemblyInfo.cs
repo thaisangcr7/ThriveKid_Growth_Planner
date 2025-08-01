f00e7d44e596410b960486e73880a73a8ea13866
sangth <thaisangcr7@gmail.com>
2025-08-01 00:31:05 -0400

    Completed tested Child, Feedinglogs, milestone,sleeplogs,

--- backend/ThriveKid.API/ThriveKid.API/obj/Debug/net8.0/ThriveKid.API.AssemblyInfo.cs
+++ backend/ThriveKid.API/ThriveKid.API/obj/Debug/net8.0/ThriveKid.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThriveKid.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8764c5f250466d18334dc32b1f79595ee5154c4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ed3c6f2e41b8f6465b725b1e17b947dfd9fe2f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThriveKid.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThriveKid.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
