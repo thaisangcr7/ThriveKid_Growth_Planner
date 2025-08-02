0f6aacfad81cac5c62a086e494e50e3421292a0c
Sang Thai <thaisangcr7@gmail.com>
2025-08-02 16:38:09 -0400

    Update .gitignore and interface for SleepLogService

--- backend/ThriveKid.API/ThriveKid.API/Services/Interfaces/ISleepLogService.cs
+++ backend/ThriveKid.API/ThriveKid.API/Services/Interfaces/ISleepLogService.cs
@@ -5,7 +5,7 @@ namespace ThriveKid.API.Services.Interfaces
     public interface ISleepLogService
     {
         Task<IEnumerable<SleepLogDto>> GetAllSleepLogsAsync();
-        Task<SleepLogDto> GetSleepLogByIdAsync(int id);
+        Task<SleepLogDto?> GetSleepLogByIdAsync(int id);
         Task<SleepLogDto> CreateSleepLogAsync(CreateSleepLogDto sleepLogDto);
         Task<bool> UpdateSleepLogAsync(int id, UpdateSleepLogDto sleepLogDto);
         Task<bool> DeleteSleepLogAsync(int id);
